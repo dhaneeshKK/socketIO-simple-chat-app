0d44e28692cdd36be3f1704411cd2a5c2d2ee82e
dhaneeshKK <dhaneesh.krishnan@gmail.com>
2023-01-18 21:45:14 -0700

    added typing... status message for other clients

--- public/chat.js
+++ public/chat.js
@@ -6,7 +6,8 @@ socket = io.connect("http://localhost:3000");
 const message = document.getElementById("message"),
 	handle = document.getElementById("handle"),
 	btn = document.getElementById("send"),
-	output = document.getElementById("output");
+	output = document.getElementById("output"),
+	feedback = document.getElementById("feedback");
 
 // Emit events
 btn.addEventListener("click", function () {
@@ -17,8 +18,17 @@ btn.addEventListener("click", function () {
 	message.value = "";
 });
 
+message.addEventListener("keypress", function () {
+	socket.emit("typing", handle.value);
+});
+
 // Listen for events
 socket.on("chat", function (data) {
+	feedback.innerHTML = "";
 	output.innerHTML +=
 		"<p><strong>" + data.handle + ": </strong>" + data.message + "</p>";
+});
+
+socket.on("typing", function (data) {
+	feedback.innerHTML = "<p><em>" + data + " is typing a message...</em></p>";
 });
